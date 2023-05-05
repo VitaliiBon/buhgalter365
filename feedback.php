1da836baba97fbc089210c930d94c07a4ebc3274
VitaliiBon <113471684+VitaliiBon@users.noreply.github.com>
2023-05-05 11:53:50 +0300

    Add files via upload

---
+++ feedback.php
@@ -0,0 +1,41 @@
+
+<?php
+$to = "bondarenkovetal5@gmail.com";//Почтовый ящик на который будет отправленно сообщение
+  $subject = "Тема сообщения";//Тема сообщения
+  $message = "Message, сообщение!";//Сообщение, письмо
+  $headers = "Content-type: text/plain; charset=utf-8 \r\n";//Шапка сообщения, содержит определение типа письма, от кого, и кому отправить ответ на письмо
+// Проверяем или метод запроса POST
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+		// Поочередно проверяем или были переданные параметры формы, или они не пустые
+		if(isset($_POST["username"]){
+			//Если параметр есть, присваеваем ему переданое значение
+			$name 		=trim(strip_tags($_POST["name"]));
+		}
+		if(isset($_POST["usernumber"]))
+		{
+			$number 	= trim(strip_tags($_POST["subject"]));
+		}
+		if (isset( $_POST["question"])) {
+			$question 	= trim(strip_tags($question));
+		}
+			// Формируем письмо
+			$message  = "<html>";
+				$message  .= "<body>";
+				$message  .= "Телефон: ".$number;
+				$message  .= "<br />";
+				$message  .= "Имя: ".$name;
+				$message  .= "<br />";
+				$message  .= "Вопрос: ".$question;
+				$message  .= "</body>";
+			$message  .= "</html>";
+			// Окончание формирования тела письма
+			// Посылаем письмо
+			mailto ($to, $subject, $message, $headers);
+}
+else
+{
+
+     
+	exit;
+} 
+?>
